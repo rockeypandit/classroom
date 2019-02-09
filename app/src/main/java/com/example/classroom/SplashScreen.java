758f79dc5f28e28cf36214c62579e83d5d5ff7ad
Rockey <rockey.pandit98@gmail.com>
2019-02-10 00:02:53 +0530

    splash screen added

--- app/src/main/java/com/example/classroom/SplashScreen.java
+++ app/src/main/java/com/example/classroom/SplashScreen.java
@@ -1,7 +1,17 @@
 package com.example.classroom;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +19,62 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ImageView imgLogo = (ImageView) findViewById(R.id.imglogo);
+        Animation slideup = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        imgLogo.startAnimation(slideup);
+        final ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        final Snackbar snackbar = Snackbar.make(imgLogo, "NO INTERNET CONNECTION", Snackbar.LENGTH_INDEFINITE);
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+
+
+                    sleep(500);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+
+
+                    while (!(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)) {
+                        //we are connected to a network
+
+                        snackbar.show();
+
+
+                    }
+
+                    snackbar.dismiss();
+
+
+                    try {
+
+
+                        sleep(400);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+
+                       if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+                           Intent gotoLogin = new Intent(SplashScreen.this, LoginActivity.class);
+                           startActivity(gotoLogin);
+                        }
+                        else {
+                           Intent gotoRegistration = new Intent(SplashScreen.this, Register.class);
+                           startActivity(gotoRegistration);
+                       }
+
+                    }
+                    finish();
+                }
+            }
+
+
+        };
+        timer.start();
     }
 }
